775827ff6383b7212d26acd4757c3e3e6891abcb
havefunatcode <dlxogh439@gmail.com>
2023-09-14 09:09:58 +0900

    ComponentScan

---
+++ src/main/java/hello/core/member/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+
+
+}
