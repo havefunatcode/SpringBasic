975f95f1de39ef61097b4214181c557c20809faa
havefunatcode <dlxogh439@gmail.com>
2023-09-18 23:12:08 +0900

    PostConstruct, PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClientTest.java
+++ src/test/java/hello/core/lifecycle/NetworkClientTest.java
@@ -20,7 +20,7 @@ class NetworkClientTest {
     @Configuration
     static class LifeCycleConfig {
 
-        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
