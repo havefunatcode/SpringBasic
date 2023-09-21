55f6b7c4014b0d3944745831a79b4e86c52db950
havefunatcode <dlxogh439@gmail.com>
2023-09-21 22:07:05 +0900

    Spring Web Request Scope

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,30 +12,25 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class AppConfig {
 
-    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
-    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), rateDiscountPolicy());
     }
 
-    @Bean
     public DiscountPolicy fixDiscountPolicy() {
         return new FixDiscountPolicy();
     }
 
-    @Bean
     public DiscountPolicy rateDiscountPolicy() {
         return new RateDiscountPolicy();
     }
